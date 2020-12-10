ec772a78ad303d2cfc33f05bae0bc0a7250b7e65
Kanegon7010 <yogaaketazeyo7010@gmail.com>
2020-12-10 16:14:46 +0900

    サインアウトの実装、deviseエラーメッセージの日本語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   
     protected
       def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
         devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+      end
+
+      def after_sign_out_path_for(resource)
+        new_user_session_path
       end
 end
