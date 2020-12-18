abf092f875496b1117c450ec7292b46a35d32991
Kanegon7010 <yogaaketazeyo7010@gmail.com>
2020-12-18 19:45:22 +0900

    フォロー機能を実装（　relationships）

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
 
   root 'homes#index'
   get 'homes/show'
-  resources :users, :only => [:index, :show]
+  resources :users, only: [:index, :show] do 
+    member do
+      get :followings, :followers
+    end
+  end 
+  resources :relationships, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/factories/relationships.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id           :bigint           not null, primary key
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  follower_id  :bigint
+#  following_id :bigint
+#
+# Indexes
+#
+#  index_relationships_on_follower_id                   (follower_id)
+#  index_relationships_on_follower_id_and_following_id  (follower_id,following_id) UNIQUE
+#  index_relationships_on_following_id                  (following_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (follower_id => users.id)
+#  fk_rails_...  (following_id => users.id)
+#
+FactoryBot.define do
+  factory :relationship do
+    follower { nil }
+    following { nil }
+  end
+end
