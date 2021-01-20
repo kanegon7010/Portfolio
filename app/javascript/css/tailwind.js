d39734c01452758c33fccb9fc9bb3b288ce15027
Kanegon7010 <yogaaketazeyo7010@gmail.com>
2021-01-20 19:23:29 +0900

    デザインを適用

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+      container: [],
+    },
     extend: {},
   },
   variants: {
